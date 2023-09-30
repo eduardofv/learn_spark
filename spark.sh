6fdeded514d21abdcd6ff38c43420cad5c8f5b07
Eduardo Flores <eduardofv@gmail.com>
2023-09-29 19:33:01 -0600

    update

--- spark.sh
+++ spark.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 8889:8888 -p 4040:4040 -v $(pwd)/work-dir:/home/jovyan/work -v /Volumes/shared/harvester/:/home/jovyan/work/harvester --name sparknb -it jupyter/pyspark-notebook\n
+docker run --rm -p 8889:8888 -p 4040:4040 -v $(pwd)/work-dir:/home/jovyan/work -v /Volumes/shared/harvester/:/home/jovyan/work/harvester --name sparknb -it jupyter/pyspark-notebook
